dd5cfb1300a52b001822c221fedd2d50e231ef2e
dev <none>
2016-03-11 21:11:37 +0530

    End of the day.

--- main_code.py
+++ main_code.py
@@ -4,17 +4,23 @@
 import dataframe
 
 from sklearn.feature_selection import RFE
+from sklearn.feature_selection import SelectKBest
+
 from sklearn.svm import SVR
 
 # The data
-x = dataframe.df_data
-y = dataframe.df_target
+x = dataframe.df_data[:60]
+y = dataframe.df_target[:60]
 
+# RFE
 estimator = SVR(kernel="linear")
-selector = RFE(estimator,5,step=1)
+selector = RFE(estimator, 5, step=1)
 
 print 'Fitting selector'
-selector = selector.fit(x,y)
+selector = selector.fit(x, y)
+
+print 'Done fitting'
 
 print "support", selector.support_
 print "ranking", selector.ranking_
+# RFE
