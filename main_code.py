0ca15dc5b146d6904002fd5ee4422defae6f98f4
PreethamKumar <kumarpreetham@outlook.com>
2016-04-29 15:20:57 +0530

    Code cleanup.

--- main_code.py
+++ main_code.py
@@ -69,62 +69,62 @@ def perform_train_test_split():
 
 
 # This is no longer needed.
-def perform_svm_test():
-    v_threshold = 0.05
-    while v_threshold < 1.0:
-        sel = VarianceThreshold(v_threshold)
-        x = sel.fit_transform(x)
-        test_x = sel.transform(test_x)
-
-        print 'Support indices', sel.get_support(indices=True)
-        print 'x size {0}, test_x size {1}'.format(len(x[0]), len(test_x[0]))
-
-        # X_train, X_test, y_train, y_test = train_test_split(new_x, y, test_size=split_ratio, random_state=0)
-
-        sc = StandardScaler()
-        sc.fit(x)
-
-        print 'Done with StandardScalar fit'
-
-        X_train_std = sc.transform(x)
-        X_test_std = sc.transform(test_x)
-
-        print 'Calling SVC'
-        svm = SVC(kernel='linear', C=1.0, random_state=0)
-        svm.fit(X_train_std, y)
-        print 'Done with svm fit'
-
-        print 'Beginning Predict...'
-        y_pred = svm.predict(X_test_std)
-        print 'Done with predict.'
-
-        print y_pred[0], y[0], test_y[0]
-
-        # print ('Misclassified samples: %d' % (test_y != y_pred).sum())
-        error_count = 0
-
-        # if len(y_pred) == len(test_y):
-        #     l = len(y_pred)
-        #
-        #     for i in range(l):
-        #         if y_pred[i] != test_y[i]:
-        #             error_count += 1
-
-        error_count = ((y_pred != test_y).sum())
-        success_count = len(test_y) - error_count
-
-        print 'Error count', error_count
-        print 'Success count', success_count
-
-        print 'Accuracy ratio of Success: %f' % ((float(success_count) / len(test_y)) * 100)
-
-        # print ('Accuracy: %.2f' % (accuracy_score(test_y, y_pred) * 100))
-        print '-' * 80
-
-        v_threshold += 0.05
-
-        x, y = dataframe.get_dataset_from_file('nsl.train')
-        test_x, test_y = dataframe.get_dataset_from_file('nsl.test')
+# def perform_svm_test():
+#     v_threshold = 0.05
+#     while v_threshold < 1.0:
+#         sel = VarianceThreshold(v_threshold)
+#         x = sel.fit_transform(x)
+#         test_x = sel.transform(test_x)
+#
+#         print 'Support indices', sel.get_support(indices=True)
+#         print 'x size {0}, test_x size {1}'.format(len(x[0]), len(test_x[0]))
+#
+#         # X_train, X_test, y_train, y_test = train_test_split(new_x, y, test_size=split_ratio, random_state=0)
+#
+#         sc = StandardScaler()
+#         sc.fit(x)
+#
+#         print 'Done with StandardScalar fit'
+#
+#         X_train_std = sc.transform(x)
+#         X_test_std = sc.transform(test_x)
+#
+#         print 'Calling SVC'
+#         svm = SVC(kernel='linear', C=1.0, random_state=0)
+#         svm.fit(X_train_std, y)
+#         print 'Done with svm fit'
+#
+#         print 'Beginning Predict...'
+#         y_pred = svm.predict(X_test_std)
+#         print 'Done with predict.'
+#
+#         print y_pred[0], y[0], test_y[0]
+#
+#         # print ('Misclassified samples: %d' % (test_y != y_pred).sum())
+#         error_count = 0
+#
+#         # if len(y_pred) == len(test_y):
+#         #     l = len(y_pred)
+#         #
+#         #     for i in range(l):
+#         #         if y_pred[i] != test_y[i]:
+#         #             error_count += 1
+#
+#         error_count = ((y_pred != test_y).sum())
+#         success_count = len(test_y) - error_count
+#
+#         print 'Error count', error_count
+#         print 'Success count', success_count
+#
+#         print 'Accuracy ratio of Success: %f' % ((float(success_count) / len(test_y)) * 100)
+#
+#         # print ('Accuracy: %.2f' % (accuracy_score(test_y, y_pred) * 100))
+#         print '-' * 80
+#
+#         v_threshold += 0.05
+#
+#         x, y = dataframe.get_dataset_from_file('nsl.train')
+#         test_x, test_y = dataframe.get_dataset_from_file('nsl.test')
 
 
 # all_test_datasets = glob.glob('C:\Users\Preetham\Documents\dataset\\nsl_independent\*')
